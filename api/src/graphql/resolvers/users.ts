b088f4511d4a0e9173ee0c77161b09a842b04382
Fausto Montiel <faustomontiel98@outlook.com>
2021-10-15 22:00:29 -0300

    Add controllers

--- api/src/graphql/resolvers/users.ts
+++ api/src/graphql/resolvers/users.ts
@@ -1,92 +1,11 @@
-import User from "../../models/User"
-import jwt from "jsonwebtoken"
-import bcrypt from "bcryptjs"
-import {UserInputError} from "apollo-server"
-import config from "../../config";
-import {validateRegisterInput,validateLoginInput} from "../../utils/validators"
-
-function generateToken(user:any) {
-    return jwt.sign(
-      {
-        id: user.id,
-        email: user.email,
-        username: user.username
-      },
-      config.JWT_SECRET_KEY,
-      { expiresIn: '1h' }
-    );
-  }
+import {getUsers,login,register} from "../../controllers/users"
 
 export = {
     Query:{
-        async getUsers(){
-            return await User.find() 
-        }
+       getUsers 
     },
     Mutation: {
-        async login(_:any,{username, password}:any) {
-
-            let { errors, valid } = validateLoginInput(username, password);
-            if (!valid) {
-              throw new UserInputError('Errors', { errors });
-            }
-      
-            const user = await User.findOne({ username });
-      
-            if (!user) {
-              errors.generals = 'User not found';
-              throw new UserInputError('User not found', { errors });
-            }
-      
-            const match = await bcrypt.compare(password, user.password);
-            if (!match) {
-              errors.generals = 'Wrong crendetials';
-              throw new UserInputError('Wrong crendetials', { errors });
-            }
-      
-            const token = generateToken(user);
-      
-            return user
-          },
-        async register(
-            _:any,
-            {
-                registerInput: { username, email, password, confirmPassword }
-            }: any
-        ) {
-            const {valid,errors} = validateRegisterInput(username, email, password, confirmPassword);
-            if(!valid){
-                console.log(valid)
-                throw new UserInputError('Errors',errors);
-            }
-
-            const user = await User.findOne({username});
-
-            if(user){
-                throw new UserInputError('Username is taken',{
-                    errors:{
-                        username: 'This username is taken'
-                    }
-                })
-            }
-
-            password = await bcrypt.hash(password, 12);
-
-            const newUser = new User({ email, username, password });
-            const res = await newUser.save();
-
-            const token = jwt.sign({
-                id: res.id,
-                email: res.email,
-                username: res.username
-            }, config.JWT_SECRET_KEY, { expiresIn: '1h' })
-
-            return {
-                email: res.email,
-                username: res.username,
-                id: res._id,
-                token
-            }
-        }
+      login,
+      register
     }
 }
